e2ca9bc2b681d09c233444b5af5506bb38c98c90
Gerardo Enrique Alvarenga <alva.92@outlook.com>
2020-07-13 19:41:37 -0400

    Minor upgrades to error handling

--- client.py
+++ client.py
@@ -12,8 +12,9 @@ import requests
 import json
 
 # TODO: Create constants for debugging levels - Replace for an actual logger
-menu_options = 0    # User selection
-
+menu_options = 0            # User selection
+menu_advance_options = 0    # User selection
+user_search = ""            # Keyword the user is looking for
 
 """
     Use a partial or full vehicle name or model to get
@@ -37,10 +38,10 @@ def get_vehicle_info(keyword):
     r = requests.get("https://swapi.dev/api/vehicles/?") # Get all vehicle data available 
     json_query_results = r.json() # Extract in JSON format
 
-    if (menu_options == '2'):
+    if (menu_options == 2):
         print("\nAll Results")
     for res in json_query_results['results']: # Go through all results
-        if (menu_options == '2'):
+        if (menu_options == 2):
             print(res)                        # Display all resutls for debugging
         
         """
@@ -51,7 +52,7 @@ def get_vehicle_info(keyword):
         name = res['name']
         model = (res['model'])
 
-        if (res['name'].find(keyword) != -1 or res['model'].find(keyword) != -1): # Check if the keyword matches a name or model
+        if (res['name'].lower().find(keyword.lower()) != -1 or res['model'].lower().find(keyword.lower()) != -1): # Check if the keyword matches a name or model
 
             """
             **********************************************************************
@@ -66,9 +67,10 @@ def get_vehicle_info(keyword):
                 match = {"name": name, "pilots": p_name}
                 results['with_pilots'].append(match)
             else:
+                # There was no pilot information found
                 results['without_pilots'].append(name) # Append only vehicle name to the dictionary        
         else:
-            if (menu_options == '2'):
+            if (menu_options == 2):
                 print("\nNo keyword match\n")
 
     """
@@ -83,34 +85,66 @@ def get_vehicle_info(keyword):
 """
 def get_pilot_information(pilot_query):
     global menu_options
-    if (menu_options == '2'):
+    if (menu_options == 2):
         print("\nGetting pilot info: " + str(pilot_query))
     pilots = []                     # List to hold the pilot's name if more than 1 is associated to a vehicle
     num_pilots = len(pilot_query)
     for x in range(num_pilots):
-        if (menu_options == '2'):
+        if (menu_options == 2):
             print("\nQuerying: " + str(pilot_query[x]))
         json_pilot_info = requests.get(pilot_query[x]).json() # Process one GET request at the tiem
         pilots.append(json_pilot_info['name'])                # Add the name to the list
 
-    if (menu_options == '2'):
+    if (menu_options == 2):
         print(pilots)
 
     return pilots
 
+"""
+    Display options to the user and prompt for a keyword to look for in the SWAPI DB
+"""
 def menu():
     print("Versature client")
     global menu_options
-    print("Select one of the options below")
-    print("1. Basic client exercise")
-    print("2. Debug client exercise")
-    print("3. Custom search")
-    menu_options = input("Selection: ")
+    global user_search
+    while(1):
+        print("Select one of the options below")
+        print("1. Basic client exercise")
+        print("2. Debug client exercise")
+        print("3. Custom search")
+        print("4. Exit")
+
+        try:    
+            menu_options = int(input("\nSelection: "))
+            if (menu_options == 4):
+                print("Bye...")
+                break
+
+            elif (menu_options == None or menu_options < 0 or menu_options > 4):
+                print("Invalid input")
+                break
+
+            user_search = input("Enter keyword to search for: ")
+        
+            if (user_search == None):
+                print("Invalid input")
+            else:
+                get_vehicle_info(user_search)
+                # TODO: Should do some better input validation - make sure that input is safe - No injections, symbols, etc.
+                print("\n")
+
+        except ValueError:
+            print("Invalid input")
+            break
+
+def advance_menu():
+    global menu_advance_options
+    print("Select one of the custom search categories below")
+    print("1. Search pilot profile")
 
 """
     Program entry point
 """
 if __name__ == "__main__":
     menu()
-    get_vehicle_info("AT-")
     pass
