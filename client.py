be0b6f5ee427d1741a748608a89d336a7d14adb8
Gerardo Enrique Alvarenga <alva.92@outlook.com>
2020-07-13 20:33:27 -0400

    Ability to search for driver's profile

--- client.py
+++ client.py
@@ -5,16 +5,22 @@
     @name         client.py
     @description  This program initializes a client to the Star Wars API (SWAPI)
     @author       Gerardo Enrique Alvarenga
-    @version      1.0
+    @version      1.1
 """
 
 import requests
 import json
 
 # TODO: Create constants for debugging levels - Replace for an actual logger
-menu_options = 0            # User selection
+menu_options         = 0    # User selection
 menu_advance_options = 0    # User selection
-user_search = ""            # Keyword the user is looking for
+user_search          = ""   # Keyword the user is looking for
+
+"""
+Supported Endpoints
+"""
+VEHICLES_ENDPOINT = "https://swapi.dev/api/vehicles/?"
+DRIVERS_ENDPOINT  = "https://swapi.dev/api/people/?"
 
 """
     Use a partial or full vehicle name or model to get
@@ -35,8 +41,8 @@ def get_vehicle_info(keyword):
         "without_pilots":[]
     }
 
-    r = requests.get("https://swapi.dev/api/vehicles/?") # Get all vehicle data available 
-    json_query_results = r.json() # Extract in JSON format
+    r = requests.get(VEHICLES_ENDPOINT) # Get all vehicle data available 
+    json_query_results = r.json()       # Extract in JSON format
 
     if (menu_options == 2):
         print("\nAll Results")
@@ -50,7 +56,7 @@ def get_vehicle_info(keyword):
         **********************************************************************
         """
         name = res['name']
-        model = (res['model'])
+        model = res['model']
 
         if (res['name'].lower().find(keyword.lower()) != -1 or res['model'].lower().find(keyword.lower()) != -1): # Check if the keyword matches a name or model
 
@@ -101,10 +107,33 @@ def get_pilot_information(pilot_query):
     return pilots
 
 """
+    Get the profile of the selected driver using search parameter provided
+    by the SWAPI instead of the filter we were using for the vehicle
+"""
+def get_full_driver_profile(pilot_name):
+    global menu_options
+    profile = {
+        "Driver":[]
+    }
+    if (menu_options == 2):
+        print("\nGetting pilot info: " + str(pilot_name))
+
+    driver = requests.get(DRIVERS_ENDPOINT +"search="+pilot_name).json() 
+    for res in driver['results']: # Go through all results
+        profile["Driver"].append(res['name'])
+        profile["Driver"].append(res['height'])
+        profile["Driver"].append(res['hair_color'])
+        profile["Driver"].append(res['skin_color'])
+        profile["Driver"].append(res['eye_color'])
+        profile["Driver"].append(res['birth_year'])
+
+    return profile
+    
+"""
     Display options to the user and prompt for a keyword to look for in the SWAPI DB
 """
 def menu():
-    print("Versature client")
+    print("\nVersature client\n")
     global menu_options
     global user_search
     while(1):
@@ -130,6 +159,7 @@ def menu():
                 print("Invalid input")
             else:
                 get_vehicle_info(user_search)
+                advance_menu()
                 # TODO: Should do some better input validation - make sure that input is safe - No injections, symbols, etc.
                 print("\n")
 
@@ -137,10 +167,23 @@ def menu():
             print("Invalid input")
             break
 
+"""
+    Second menu which allows the user to search for the profile of the driver
+    @TODO: Needs error handling
+"""
 def advance_menu():
     global menu_advance_options
-    print("Select one of the custom search categories below")
+    print("\nSelect one of the custom search categories below")
     print("1. Search pilot profile")
+    print("2. Run another search")
+    menu_advance_options = int(input("\nSelection: "))
+    if (menu_advance_options == 1):
+        pilot_search = input("Enter the pilot name: ")
+        pilot_res = get_full_driver_profile(pilot_search)
+        print(pilot_res)
+
+    elif (menu_advance_options == 2):
+        menu()
 
 """
     Program entry point
